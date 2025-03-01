93b47fb0992c77f54c7ee400a0aba6e867ab0ea6
kunaldec022002 <kunalingale72@gmail.com>
2025-03-01 22:59:05 +0530

    50 % work are completed

--- src/component/NewsArticle/NewsArticle.jsx
+++ src/component/NewsArticle/NewsArticle.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import './NewsArticle.css'
 
-export default function NewsArticle() {
+ function NewsArticle({author,title,description,url,urlToImage,publishedAt}) {
   return (
     <div>
-      <h2>NewsArticle</h2>
+      
+
+      <div className='news-article-card'>
+        <img src={urlToImage} className='news-article-img' alt='url-img'/>
+        <h1 className='article-title'>{title}</h1>
+
+        <div className='article-info'>
+          <p className='article-author'>{author}</p>
+          <p className='article-publishedAt'>{publishedAt}</p>
+        </div>
+
+        <p className='article-description'>{description}</p>
+        <a href={url} target='_blank' rel="noreferrer" className='btn-read-more'>Read More</a>
+      </div>
       
     </div>
   )
 }
+
+export default NewsArticle;
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import NewsArticle from '../../component/NewsArticle/NewsArticle';
+import './Home.css'
 
 export default function Home() {
+
+  const [news, setNews]=useState([]);
+
+  const loadNews = async ()=>{
+    try{
+      const response = await axios.get("https://newsapi.org/v2/everything?q=apple&from=2025-02-28&to=2025-02-28&sortBy=popularity&apiKey=70444052d2d2459c99023e4532137427");
+
+      setNews(response.data.articles);
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
+
+  useEffect (()=>{
+    loadNews()
+  },[])
+
   return (
     <div>
       <h2>Home section</h2>
+
+      <div className='news-container'>
+        {
+           news.map((newsArticle,index)=>{
+            const {author,title,description,url,urlToImage,publishedAt}= newsArticle
+
+            return(
+              <NewsArticle author={author} title={title} description={description} url={url} urlToImage={urlToImage} publishedAt={publishedAt} key={index}/>
+            )
+           })
+        }
+      </div>
       
     </div>
   )
