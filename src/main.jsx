d6f63c75a06a8cbf505aa0f1eb97319770722551
kunaldec022002 <kunalingale72@gmail.com>
2025-03-02 15:32:44 +0530

    complete news website

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import Home from './views/Home/Home'
 
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Home />
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,13 +3,16 @@ import axios from 'axios'
 import NewsArticle from '../../component/NewsArticle/NewsArticle';
 import './Home.css'
 
+
 export default function Home() {
 
   const [news, setNews]=useState([]);
 
+  const [searchQuery, setSearchQuery]= useState('Delhi')
+
   const loadNews = async ()=>{
     try{
-      const response = await axios.get("https://newsapi.org/v2/everything?q=apple&from=2025-02-28&to=2025-02-28&sortBy=popularity&apiKey=70444052d2d2459c99023e4532137427");
+      const response = await axios.get(`https://newsapi.org/v2/everything?q=${searchQuery}&from=2025-02-28&to=2025-02-28&sortBy=popularity&apiKey=70444052d2d2459c99023e4532137427`);
 
       setNews(response.data.articles);
     }
@@ -20,11 +23,20 @@ export default function Home() {
 
   useEffect (()=>{
     loadNews()
-  },[])
+  },)
+
+  useEffect(()=>{
+    loadNews()
+  },)
 
   return (
     <div>
-      <h2>Home section</h2>
+      <div>
+        <input type='search' className='input-search' placeholder='Enter any news' onChange={(e)=>{
+          setSearchQuery(e.target.value)
+        }}/>
+      </div>
+    
 
       <div className='news-container'>
         {
